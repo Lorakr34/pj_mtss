c74af262b9bfa6121c7d9e041d9ec5f28fc24785
mmonika2908 <u.monika2908@gmail.com>
2026-05-04 15:39:37 +0200

    Update IntegerToRoman.java
    Extend valid range from 3 to 9

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,7 +6,7 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
     public static String convert(int number){
-        if (number < 1 || number > 3) {
+        if (number < 1 || number > 9) {
             return null; 
         }
         
