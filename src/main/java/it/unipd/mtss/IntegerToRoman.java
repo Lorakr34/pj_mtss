4688860d17acb544167297d6dd93465db2e0bff6
Lorakr34 <gsi16astra0@gmail.com>
2026-05-04 13:54:32 +0200

    Completata Issue 2: conversione numeri da 1 a 3

---
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////
+// Carol Gardin 2147962
+// Monika Ullah 2116413
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+public class IntegerToRoman {
+    public static String convert(int number){
+        if (number < 1 || number > 3) {
+            return null; 
+        }
+        
+        StringBuilder roman = new StringBuilder();
+        while (number >= 1) {
+            roman.append("I");
+            number = number - 1;
+        }
+        return roman.toString();
+    }
+}
