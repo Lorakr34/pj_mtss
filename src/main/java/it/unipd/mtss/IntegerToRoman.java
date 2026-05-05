be102344e5f9a6dd0f0bc174bd275dbcc58168a9
mmonika2908 <u.monika2908@gmail.com>
2026-05-05 10:47:15 +0200

    Add support for odd numbers(5, 9, 11)
    Added special cases for 9 (IX) and 11 (XI) and support for 5 (V) while keeping original logic unchanged.

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -11,6 +11,21 @@ public class IntegerToRoman {
         }
         
         StringBuilder roman = new StringBuilder();
+
+        //Monika - numeri dispari
+        if (number == 9){
+            return "IX";
+        }
+
+        if (number == 11){
+            return "XI";
+        }
+
+        if (number >= 5){
+            roman.append("V");
+            number = number - 5;
+        }
+
         while (number >= 1) {
             roman.append("I");
             number = number - 1;
