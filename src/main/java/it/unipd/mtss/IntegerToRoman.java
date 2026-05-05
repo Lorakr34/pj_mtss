de8235bca9c3650d0bd94f5b07c8c55dbce30394
mmonika2908 <u.monika2908@gmail.com>
2026-05-05 11:03:10 +0200

    Implement conversion for first six numbers

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,21 +6,17 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
     public static String convert(int number){
-        if (number < 1 || number > 11) {
+        if (number < 1 || number > 6) {
             return null; 
         }
         
         StringBuilder roman = new StringBuilder();
 
-        //Monika - numeri dispari
-        if (number == 9){
-            return "IX";
+        if(number == 4){
+            return "IV";
         }
 
-        if (number == 11){
-            return "XI";
-        }
-
+        
         if (number >= 5){
             roman.append("V");
             number = number - 5;
